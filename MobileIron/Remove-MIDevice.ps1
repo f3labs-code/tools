8dfc82f0fe4470bd722bd24541727487616217b1
Michael Ross <mross@f3partners.com>
2021-10-13 12:52:33 -0400

    updated script to fix errors

--- MobileIron/Remove-MIDevice.ps1
+++ MobileIron/Remove-MIDevice.ps1
@@ -1,19 +1,25 @@
 Write-Host "Enter credentials for MobileIron:" -ForegroundColor Blue
-$creds = Get-Credential
+$cred = Get-Credential
 
 $csvfile = '.\devices_to_retire.csv'
 
 $emails = import-Csv $csvfile
 
+# Get dmPartition Id
+$dmPartitionId = (Invoke-RestMethod -Method 'Get' -Uri https://na1.mobileiron.com/api/v1/metadata/tenant -Credential $cred -Authentication Basic).result.defaultDmPartitionId
+write-host "dmPartitionId: $dmPartitionId"
+
 foreach ($email in $emails) {
     $smtp = $email.primarySmtpAddress
+    write-host "Checking $smtp for MobileIron Devices"
 
-    $devices = (Invoke-RestMethod -Method 'Get' -Uri 'https://na1.mobileiron.com/api/v1/device?q=&dmPartitionId=26653&fq=EMAILADDRESS+EQ+sjobs@chesuro.com' -Credential $cred -Authentication Basic).result.searchResults
+    $devices = (Invoke-RestMethod -Method "Get" -Uri "https://na1.mobileiron.com/api/v1/device?q=&dmPartitionId=$dmPartitionId&fq=EMAILADDRESS+EQ+$smtp" -Credential $cred -Authentication Basic).result.searchResults
     if ($devices -eq $null) {
         write-host "User $smtp did not have any devices!" -ForegroundColor Yellow
-        break
+        continue
     }
     foreach ($device in $devices) {
+        write-host "Checking $smtp device $($device.prettyModel)"
         if ($device.registrationState -ne "RETIRED" -and $device.registrationState -ne "PENDING RETIREMENT") {
             $deviceId = $device.id
             $deviceModel = $device.prettyModel
@@ -22,6 +28,8 @@ foreach ($email in $emails) {
             if ($result.error -eq $null) {
                 write-host "Successfully retired device $deviceModel for user $smtp" -ForegroundColor Green
             }
+        } else {
+            write-host "Device $($device.prettyModel) already retired. Skipping..."
         }
     }
 
